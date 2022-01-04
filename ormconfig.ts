f1be42495ee80c1aff74671a1e52c6ba04e80a49
tornikes <t.sadghobelashvili@gmail.com>
2022-01-04 16:16:14 +0400

    init: setup empty project

---
+++ ormconfig.ts
@@ -0,0 +1,22 @@
+import { config } from "dotenv";
+config();
+
+export default {
+  synchronize: false,
+  logging: true,
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscriber/**/*.ts"],
+  cli: {
+    entitiesDir: "src/entity",
+    migrationsDir: "src/migration",
+    subscribersDir: "src/subscriber",
+  },
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "yelp_like",
+  migrationsTableName: "custom_migration_table",
+};
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import express from "express";
+
+const app = express();
+
+async function start() {
+  const conn = await createConnection();
+
+  const PORT = process.env.PORT || 5000;
+
+  app.get("/", (req, res) => {
+    res.send({ message: "Hello, World" });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server launched at port ${PORT}`);
+  });
+}
+
+start();
