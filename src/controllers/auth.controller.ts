e227776c0d226f4975450ab62d6167329cc50ef1
tornikes <t.sadghobelashvili@gmail.com>
2022-01-04 19:47:33 +0400

    feat: return jwt to user

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { getManager } from "typeorm";
 import { User } from "../entity/User";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 const authRouter = express.Router();
 
 authRouter.post("/register", async (req, res) => {
@@ -17,6 +19,30 @@ authRouter.post("/register", async (req, res) => {
   await em.save(user);
 
   res.status(201).send({ user: copiedUser });
+});
+
+authRouter.post("/login", async (req, res) => {
+  const em = getManager();
+  console.log(req.body);
+
+  const user = await em.findOne(User, { email: req.body.email });
+
+  if (!user) {
+    return res.status(404).send({ message: "unknown user" });
+  }
+
+  const isMatch = await bcrypt.compare(req.body.password, user.passwordHash);
+  if (!isMatch) {
+    return res.send({ message: "Invalid credentials" });
+  }
+
+  const userData = { id: user.id, name: user.userName };
+
+  const token = jwt.sign(userData, "SUPER_SECRET", {
+    expiresIn: "7d",
+  });
+
+  res.send({ token });
 });
 
 export default authRouter;
